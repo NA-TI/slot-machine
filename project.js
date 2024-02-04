ce46f23b81fdbcc3984611c4adfa9c10bfbef957
Nati <natihabtamu199@gmail.com>
2024-02-04 05:14:04 +0200

    first commit

---
+++ project.js
@@ -0,0 +1,13 @@
+// 1. despost some money
+// 2. determine number of lines to bet on
+// 3. collect a bet amount
+// 4. spin the slot machine
+// 5. check if the user won
+// 6. give the user their winning
+// 7. play again
+
+function deposite() {
+    return 1
+}
+
+const x = deposite()

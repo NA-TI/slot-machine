1eeae11d58b8f6b1fc215fcc0712e58de4264211
NA-TI <natihabtamu199@gmail.com>
2024-02-04 06:32:05 +0200

    overall

--- project.js
+++ project.js
@@ -6,8 +6,53 @@
 // 6. give the user their winning
 // 7. play again
 
-function deposite() {
-    return 1
-}
+const prompt = require("prompt-sync")();
 
-const x = deposite()
+
+
+
+
+
+const getDepositAmount = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount, try again.");
+        } else {
+            return numberDepositAmount;
+        }
+    }
+};
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
+const getBet = (balance, lines) => {
+        while (true) {
+            const bet = prompt("Enter the bet per line: ");
+            const numberOfBet = parseFloat(bet);
+    
+            if (isNaN(numberOfBet) || numberOfBet <= 0 || numberOfBet > balance /  lines) {
+                console.log("Invalid bet, try again.");
+            } else {
+                return numberOfbet;
+            }
+        }
+};
+
+let balance = getDepositAmount();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+
